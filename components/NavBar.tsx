29974aff736e691c7127ffaf33a0ec1abe3c6712
Mike Cecconello <mikececco2000@gmail.com>
2024-08-06 23:29:02 +0200

    navbar items

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,8 +3,13 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { Icons } from './Icons'
 import NavItems from './NavItems'
+import { buttonVariants } from "@/components/ui/button";
+import Cart from './Cart'
 
 const NavBar = () => {
+
+  const user = null
+
   return (
     <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
       <header className='relative bg-white'>
@@ -18,6 +23,24 @@ const NavBar = () => {
               </div>
               <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
                 <NavItems />
+              </div>
+
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  { user ? null : <Link href='/sign-in' className={ buttonVariants({variant: "ghost"}) }>Sign in</Link> }
+                  { user ? null : <span className='h-6 w-px bg-gray-200' aria-hidden="true" /> }
+                  { user ? null : <Link href='/sign-up' className={ buttonVariants({variant: "ghost"}) }>Create account</Link> }
+                  { user ? null : <span className='h-6 w-px bg-gray-200' aria-hidden="true" /> }
+                  { user ? null : (
+                    <div className='flex lg:ml-6'>
+                        { user ? null : <span className='h-6 w-px bg-gray-200' aria-hidden="true" /> }
+                      </div>
+                    )
+                  }
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
               </div>
               {/* TODO: Mobile Nav */}
             </div>
