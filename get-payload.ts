b064c1f5967b647ef5442bb6860e20028b99c34d
Mike Cecconello <mikececco2000@gmail.com>
2024-08-07 21:17:52 +0200

    Database setup payload

---
+++ get-payload.ts
@@ -0,0 +1,40 @@
+import type { InitOptions } from "payload/config"
+import payload from "payload"
+
+let cached = (global as any).payload
+
+if (!cached) {
+  cached = (global as any).payload  = {
+    client: null,
+    promise: null,
+  }
+}
+interface Args {
+  initOptions?: Partial<InitOptions>
+}
+
+export const getPayloadClient = async ({initOptions}: Args = {}) => {
+  if(process.env.PAYLOAD_SECRET) {
+    throw new Error('Payload secret missing')
+  }
+
+  if(cached.client){
+    return cached.client
+  }
+
+  if (cached.promise && process.env.PAYLOAD_SECRET != undefined) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {})
+    })
+  }
+
+  try {
+    cached.client = await cached.promise
+  } catch (e: unknown) {
+    cached.promise = null
+    throw e
+  }
+  return cached.client
+}
